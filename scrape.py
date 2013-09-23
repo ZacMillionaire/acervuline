64c26662f74aaa613ce4fed2cc706e96a5d27277
Scott Schultz <dev@jotunga.com>
2013-09-23 10:26:55 +1000

    Fixed value collection
    * Added some tests
    * Fixed field detection to actually get data
    * Turns out that is pretty important.

--- scrape.py
+++ scrape.py
@@ -98,6 +98,8 @@ def scrape_unit_info(unit_list):
 
 	unit_info_list = []
 
+	unit_info_list.append(unit_list)
+
 	course_offer_regex = re.compile("<th>Courses unit offered in:</th>.*?<td>(.*?)</td>",re.I|re.M|re.S)
 	unit_info_list.append(course_offer_regex.findall(data_buffer))
 
@@ -109,6 +111,9 @@ def scrape_unit_info(unit_list):
 	
 	equiv_regex = re.compile("<th>Equivalent\(s\):</th>.*?<td>(.*?)</td>",re.I|re.M|re.S)
 	unit_info_list.append(equiv_regex.findall(data_buffer))
+
+	other_req_regex = re.compile("<th>Other requisite\(s\):.*?</th>.*?<td>(.*?)</td>",re.I|re.M|re.S)
+	unit_info_list.append(other_req_regex.findall(data_buffer))
 	
 	credit_regex = re.compile("<th>Credit points:</th>.*?<td>(.*?)</td>",re.I|re.M|re.S)
 	unit_info_list.append(credit_regex.findall(data_buffer))
@@ -119,13 +124,13 @@ def scrape_unit_info(unit_list):
 	study_period_regex = re.compile("<th>Teaching period:</th>.*?<td>(.*?)</td>",re.I|re.M|re.S)
 	unit_info_list.append(study_period_regex.findall(data_buffer))
 
-	csp_regex = re.compile("<th>CSP student contribution</th>.*?<td>(.*?)</td>",re.I|re.M|re.S)
+	csp_regex = re.compile("<th>CSP student contribution.*?</th>.*?<td>(.*?)</td>",re.I|re.M|re.S)
 	unit_info_list.append(csp_regex.findall(data_buffer))
 
-	domestic_fee_regex = re.compile("<th>Domestic tuition unit fee</th>.*?<td>(.*?)</td>",re.I|re.M|re.S)
+	domestic_fee_regex = re.compile("<th>Domestic tuition unit fee.*?</th>.*?<td>(.*?)</td>",re.I|re.M|re.S)
 	unit_info_list.append(domestic_fee_regex.findall(data_buffer))
 
-	international_fee_regex = re.compile("<th>International unit fee</th>.*?<td>(.*?)</td>",re.I|re.M|re.S)
+	international_fee_regex = re.compile("<th>International unit fee.*?</th>.*?<td>(.*?)</td>",re.I|re.M|re.S)
 	unit_info_list.append(international_fee_regex.findall(data_buffer))
 	
 	synopsis_regex = re.compile("<H3>Synopsis:</H3>.*?<p>(.*?)</p>",re.I|re.M|re.S)
@@ -139,8 +144,8 @@ def scrape_unit_info(unit_list):
 		except IndexError:
 			unit_info_list[key] = 0
 		else:
-			pattern = re.compile(r'&nbsp;+')
-			sentence = re.sub(pattern, ' ', info_entry[0])
+			pattern = re.compile(r'&nbsp;|&amp;+')
+			sentence = re.sub(pattern, '', info_entry[0])
 			unit_info_list[key] = sentence.strip()
 	return unit_info_list
 
@@ -158,10 +163,31 @@ def dump_to_file(lists,names):
 	output_file.write(data_buffer)
 	output_file.close()
 
-# A semblance of a test
-# unit_info = scrape_unit_info(['43713', 'MGN534', 'http://www.qut.edu.au/study/unit-search/unit?idunit=43713'])
-# print unit_info
-# exit()
+# HEY GUESS WHAT WOULD'VE PROBABLY SAVED A 6 HOUR EXECUTION? UNIT TESTING!
+# I'm still not going to put them in proper though. Of course.
+
+# Test 1
+# expects:
+#['46436', 0, 0, 0, 0, 'An application, interview and subsequent approval by the Unit Coordinator is required to enrol in this unit. In addition to completion of the following units: AYN417  AYN418.', '12', 'Gardens Point', '2013 SEM-1 and 2013 SEM-2', '$1,224', '$2,544', '$3,072', 'This unit fosters learning through work related experience. Students will be given the opportunity to experience the work that is performed by accountants which will enable them to more effectively learn and practice accounting discipline knowledge and graduate capabilities. Admission to this unit is by application and subsequent approval by the unit coordinator. <br><br>For additional important information about this unit please refer to the current unit outline.']
+unit_info = scrape_unit_info(['46436', 'AYN460', 'http://www.qut.edu.au/study/unit-search/unit?idunit=46436'])
+print unit_info
+exit()
+
+
+# Test 2
+# expects:
+#['43713', 'BS92, IF49', 0, 0, 'GSN234', 0, '12', 'Gardens Point', '2013 SEM-1', '$1,224', '$2,544', '$3,072', 'The unit introduces the students to the field of entrepreneurship research and the problems, theories and methods that are prevalent in (empirical) research on entrepreneurship. Students learn to "know the field" including its historical development; its "infrastructure" of journals, conferences and research centres, and its contemporary research questions and approaches. The students will develop an ability to assess the strengths and weaknesses of the field and gain insights into where and how they can contribute to its research frontier.']
+unit_info = scrape_unit_info(['43713', 'MGN534', 'http://www.qut.edu.au/study/unit-search/unit?idunit=43713'])
+print unit_info
+exit()
+
+
+# Test 3
+# expects:
+#['45637', 0, 'BSB111 or CTB111', 'LWB364', 'AYB325, AYX219', 0, '12', 'Gardens Point', '2013 SEM-1 and 2013 SEM-2', '$1,224', '$2,136', '$2,868', "This unit introduces students to the statutory framework of the Australian taxation system. Elements in the determination of taxable income and the levy of income tax are examined including general and specific categories of assessable income and allowable deductions, capital gains tax and administration aspects of the tax system. The taxation of fringe benefits is also examined. The unit also provides  a brief overview of the taxation of partnerships, trusts and companies and an overview of the goods and services tax. Emphasis is placed on developing students' skills in problem solving through research and analysis of taxation issues."]
+unit_info = scrape_unit_info(['45637', 'AYB219', 'http://www.qut.edu.au/study/unit-search/unit?unitCode=AYB219&idunit=45637'])
+print unit_info
+exit()
 
 
 # And all this shit below is a horrible mess I just wanted it to work for now
@@ -204,7 +230,7 @@ print "...Finished.\n"
 
 print "I hope you have a lot of time on your hands cause this'll take a few years."
 for i,unit in enumerate(unit_list):
-	print "\tScraping data for unit code %s (item %d of %d (%f %%))"%(unit[1],(i+1),len(unit_list),(float(i+1)/len(unit_list))*100) # Guess what should've ACTUALLY been a float?
+	print "\tScraping data for unit code %s (item %d of %d (%f%%))"%(unit[1],(i+1),len(unit_list),(float(i+1)/len(unit_list))*100) # Guess what should've ACTUALLY been a float?
 	scraped_data = scrape_unit_info(unit)
 	print "\tData Collected. Adding %s..."%(unit[1])
 	with open('scraped_data.ohgodwhy','a') as dump_file:
